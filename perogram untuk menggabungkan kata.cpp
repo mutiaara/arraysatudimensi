c5f696839d2960be75c8266672003d767a412ac4
mutiaa <141219162+mutiaara@users.noreply.github.com>
2023-08-24 11:26:11 +0700

    Add files via upload

---
+++ perogram untuk menggabungkan kata.cpp
@@ -0,0 +1,19 @@
+#include <cstdlib>
+#include <iostream>
+#include <cstring>
+using namespace std;
+int main()
+{
+//mendeklarasikan teks1 dan teks2
+  char teks1[10], teks2 [10];
+//menampilkan pesan u/ meminta masukan dari pengguna
+  cout << "Masukkan Nama Depan Anda?: ";
+  cin >> teks1;
+  cout << "Masukkan Nama Keluarga Anda?: ";
+//menggabungkan teks 2 ke teks1
+  strcat(teks1, teks2);
+//menampilkan hasil ke pengguna 
+  cout << "Mana lengkap Anda: " << teks1 << endl;
+  system ("PAUSE");
+  return 0; 
+}
